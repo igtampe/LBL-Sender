7688fdf692101dc8d92b40eb4df661d0fcfd7ff4
Ignacio Tampe <Igtampe@outlook.com>
2020-04-01 22:46:06 -0400

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.Text = "Press me again! An LBL Server should be able to handle more than one LBL transfer at a time"
+        OpenFileDialog1.Multiselect = True
+        Dim result As Boolean = OpenFileDialog1.ShowDialog()
+        If result Then
+            For Each Filename As String In OpenFileDialog1.FileNames
+                Dim LBLER As LBLSender = New LBLSender With {.FileToSend = Filename}
+                LBLER.Show()
+            Next
+        End If
+    End Sub
+
+    Private Sub Button1_MouseDown(sender As Object, e As MouseEventArgs) Handles Button1.MouseDown
+        Button1.Text = "now let me go!"
+    End Sub
+End Class
---
+++ ServerCommand.vb
@@ -0,0 +1,58 @@
+﻿Imports System.IO
+Imports System.Net.Sockets
+
+''' <summary>
+''' ServerCommand is a utility that sends a command to the ViBE Server or an Equivalent to the ViBE Server
+''' </summary>
+Public Class ServerCommand
+
+    Const IP As String = “Localhost”
+    Public SessionID As String = ""
+
+    Public Shared Function RawCommand(ByVal ClientMSG As String, Optional IPOverride As String = "") As String
+
+        Dim tc As TcpClient = New TcpClient()
+        Dim ns As NetworkStream
+        Dim br As BinaryReader
+        Dim bw As BinaryWriter
+        Dim ServerMSG As String
+
+
+        ServerMSG = "E"
+
+        'Check if the ClientMSG is blank, and if it is return an error flag
+        If String.IsNullOrEmpty(ClientMSG.Trim) Then
+            Throw New Exception("It is vacio. Why did u send something vacio")
+        End If
+
+        Try
+            If Not String.IsNullOrEmpty(IPOverride.Trim) Then
+                tc.Connect(IPOverride, 757)
+            Else
+                tc.Connect(IP, 757)
+            End If
+        Catch
+            MsgBox("Unable to connect to the server.", MsgBoxStyle.Exclamation, "ViBE Error")
+
+        End Try
+
+        If tc.Connected = True Then
+            ns = tc.GetStream
+            br = New BinaryReader(ns)
+            bw = New BinaryWriter(ns)
+
+            bw.Write(ClientMSG)
+
+            Try
+                ServerMSG = br.ReadString()
+            Catch
+                MsgBox("Seems like the server might've crashed! Contact CHOPO!", MsgBoxStyle.Exclamation, "ViBE Error")
+                Return "CRASH"
+            End Try
+
+            tc.Close()
+            Return ServerMSG
+        End If
+        Return ServerMSG
+    End Function
+End Class
